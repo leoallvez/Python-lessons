b2c34e7b2cc8008ae75272c3bfdb51872557ec69
leoallvez <leoallvez@hotmail.com>
2018-04-03 01:28:39 -0300

    Average Programs: Casa do Código

---
+++ 033-First-Complete-Program.py
@@ -0,0 +1,45 @@
+
+import io
+import sys
+import urllib.request as request
+
+BUFF_SIZE = 1024
+
+
+def download_length(response, output, length):
+    times = length  # BUFF_SIZE
+    if length % BUFF_SIZE > 0:
+        times += 1
+    for time in range(times):
+        output.write(response.read(BUFF_SIZE))
+        print('Download {}'.format((time * BUFF_SIZE) / length) * 100)
+
+
+def download(response, out_file):
+    total_downloaded = 0
+    while True:
+        data = response.read(BUFF_SIZE)
+        total_downloaded += len(data)
+        if not data:
+            break
+        out_file.write(data)
+        print(" Download {bytes}\n".format(bytes=total_downloaded))
+
+
+def main():
+    #  response = request.urlopen(sys.argv[1])
+    response = request.urlopen('http://livropython.com.br/dados.zip')
+    out_file = io.FileIO("saida.zip", mode="w")
+    content_length = response.getheader('Content-Length')
+    if content_length:
+        length = int(content_length)
+        download_length(response, out_file, length)
+    else:
+        download(response, out_file)
+    response.close()
+    out_file.close()
+    print('Finished')
+
+
+if __name__ == '__main__':
+    main()
---
+++ 034-Extract-Zip.py
@@ -0,0 +1,17 @@
+import os
+import zipfile
+import sys
+
+
+def main(path):
+    if not os.path.exists(path):
+        print('Arquivo {} não existe'.format(path))
+        sys.exit(-1)
+    else:
+        zfile = zipfile.ZipFile(path)
+        zfile.extractall()
+        print('Arquivo(s) extraído(s)')
+
+
+if __name__ == "__main__":
+    main('C:/Users/Leonardo/PycharmProjects/python-first-steps/saida.zip')
